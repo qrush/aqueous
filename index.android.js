f0398fdeeeb8ca8cf4eb9d67893cb57f08aa9480
Nick Quaranto <nick@quaran.to>
2015-11-22 22:13:28 -0500

    Initial example import

---
+++ index.android.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry
+} = React;
+
+var Aqueous = require('./home.js');
+
+AppRegistry.registerComponent('Aqueous', () => Aqueous);
